0e39ed5c6e34c0e9b1a91b065aef5e2f06df380b
Hionel <104563991+Hionel@users.noreply.github.com>
2023-07-05 22:18:02 +0300

    validate signed in jwt token implemented on routes

--- router/user-router.js
+++ router/user-router.js
@@ -3,28 +3,40 @@ import * as userController from "../controllers/user-controller.js";
 import * as validationMiddleware from "../middleware/validation-middleware.js";
 
 const userRouter = express.Router();
-
+// User routes
 userRouter.get(
 	"/",
+	validationMiddleware.validateSignToken,
 	validationMiddleware.validateAdminPermission,
 	userController.getAllUsers
 );
-userRouter.get("/:id", userController.getUserById);
+userRouter.get(
+	"/:id",
+	validationMiddleware.validateSignToken,
+	userController.getUserById
+);
 userRouter.patch(
 	"/:id",
+	validationMiddleware.validateSignToken,
 	validationMiddleware.validateUserUpdate,
 	userController.updateUserById
 );
 userRouter.delete(
 	"/:id",
+	validationMiddleware.validateSignToken,
 	validationMiddleware.validateAdminPermission,
 	userController.deleteUser
 );
+// Auth routes
 userRouter.post(
 	"/",
 	validationMiddleware.validateUserRegistration,
 	userController.createUser
 );
-userRouter.post("/login");
+userRouter.post(
+	"/login",
+	validationMiddleware.validateLogin,
+	userController.login
+);
 
 export default userRouter;
